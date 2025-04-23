422d701c2f4713abc9ab36784cc39f140667e4ad
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-04-23 16:25:10 -0300

    Criação de Repositories

---
+++ src/main/java/com/devsuperior/movieflix/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.movieflix.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.movieflix.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
